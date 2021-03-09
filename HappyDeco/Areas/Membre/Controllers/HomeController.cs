c910f62fb299773c01b6afbe41c05335657cab89
coudert <paulinecoudert@gmail.com>
2021-03-09 17:41:41 +0100

    non resolu upload

--- HappyDeco/Areas/Membre/Controllers/HomeController.cs
+++ HappyDeco/Areas/Membre/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace HappyDeco.Areas.Membre.Controllers
         public ActionResult Profil()
         {
             ViewBag.Message = " ";
-          
+
 
             return View();
         }
@@ -38,61 +38,56 @@ namespace HappyDeco.Areas.Membre.Controllers
             return RedirectToAction("Index", "Home", new { area = "" });
         }
 
-        public ActionResult Picture()
-        {
-            return View(SessionUtil.ConnectedProjet);
-        }
 
 
         [HttpPost]
-        public ActionResult Index(ProjetModel projet,  HttpPostedFileBase FilePicture) { 
+        public ActionResult Index(ProjetModel projet, HttpPostedFileBase FilePicture)
+        {
+            //1- vérifier que la photo à une taille supérieure à 0 et pas trop lourde <200Mo
+            if (FilePicture.ContentLength > 0 && FilePicture.ContentLength < 20000)
+            {
+                //2 Vérifier le type
+                string extension = Path.GetExtension(FilePicture.FileName);
+                if (valideImageType.Contains(extension))
+                {
+                    //3 vérifier si le dossier de destination existe
+                    //D:\Cours\Wad20\NetFlask\images\Users\1
+                    string destFolder = Path.Combine(Server.MapPath("~/images/portfolio"), SessionUtil.ConnectedProjet.IdProjet.ToString());
+                    if (!Directory.Exists(destFolder))
+                    {
+                        Directory.CreateDirectory(destFolder);
+                    }
+
+                    //4 - Upload de l'image
+                    FilePicture.SaveAs(Path.Combine(destFolder, FilePicture.FileName));
+
+                    //5 Mise à jour de l'objet User
+                    SessionUtil.ConnectedProjet.Image = FilePicture.FileName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
 
             if (ModelState.IsValid)
             {
-
-                //Juste pour démontrer l'upload de photo 
-                //1- vérifier que la photo à une taille supérieure à 0 et pas trop lourde <200Mo
-                if (FilePicture.ContentLength > 0 && FilePicture.ContentLength < 20000)
-                {
-                    //2 Vérifier le type
-                    string extension = Path.GetExtension(FilePicture.FileName);
-                    if (valideImageType.Contains(extension))
-                    {
-                        //3 vérifier si le dossier de destination existe
-                        //D:\Cours\Wad20\NetFlask\images\Users\1
-                        string destFolder = Path.Combine(Server.MapPath("~/images/portfolio"), SessionUtil.ConnectedProjet.IdProjet.ToString());
-                        if (!Directory.Exists(destFolder))
-                        {
-                            Directory.CreateDirectory(destFolder);
-                        }
-
-                        //4 - Upload de l'image
-                        FilePicture.SaveAs(Path.Combine(destFolder, FilePicture.FileName));
-
-                        //5 Mise à jour de l'objet User
-                        SessionUtil.ConnectedProjet.Image = FilePicture.FileName;
-
-                    }
-                }
-
                 DataContext ctx = new DataContext(ConfigurationManager.ConnectionStrings["Cnstr"].ConnectionString);
                 if (ctx.SaveProjet(projet))
                 {
-                   
 
-                            return RedirectToAction("Realisations", "Home", new { area = "" });
+
+                    return RedirectToAction("Realisations", "Home", new { area = "" });
                 }
                 else
                 {
                     return View();
                 }
+
             }
             else
             {
                 return View();
             }
         }
-
 
     }
 }
